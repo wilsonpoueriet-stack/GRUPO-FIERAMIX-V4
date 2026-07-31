8dbffb3c2af70cad20d33404c7ee862ba039f305
Wilson Poueriet <wilsonpoueriet@gmail.com>
2026-07-31 07:42:55 -0400

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    formats: ["image/avif", "image/webp"]
-  }
+  /* config options here */
 };
 
 export default nextConfig;
